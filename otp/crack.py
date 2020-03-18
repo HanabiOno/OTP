5d5f28b349e9493b61f8528761f1e0bb2591a190
Hanabi Ono <Hanabi.ob@gmail.com>
2020-03-18 18:26:53 +0100

    started on addngramtod function

--- otp/crack.py
+++ otp/crack.py
@@ -46,10 +46,28 @@ def addwordtod(d, word):
     """This function adds [word] to all possible places in [d(ifference)] and gives all those outputs
 Input = difference of two cypher texts (with same otp)
 Output = dict where keys are start of difference where word is added and values are the added outcome  """
-    dict = {}
+    list1 = []
     for i in range(0,len(d)-(len(word)-1)):
-        dict[i]=add_modulo_alphabet(d[i:i+len(word)], word)
-        print(dict[i])
+        list1.append(add_modulo_alphabet(d[i:i+len(word)], word))
+    return list1
 
+def addngramtod(d, txt, T=1, n=3):
+    """Doesn't work yet! Tries addition of common english ngrams to difference until T (threshold) of the outcomes is also a common english ngram. Threshhold is standard set to 1 and n-gram to 3"""
+    with open(txt, 'r') as txt:
+        for ngram in txt:
+            i = 0
+            "when there are T common english n-grams found we stop"
+            "ngram[0:n] because the file also contains numbers after each ngram"
+            out = addwordtod(d, ngram[0:n])
+            while i < T:
+                for ngram_out in out:
+                    "We want to save the index of the output n-gram if it is a common english n-gram"
+                    idx_ngram = []
+                    if ngram_out in txt:
+                        ngrams_out.append(out.index(ngram_out))
+                        i += 1
+                    else:
+                        continue
+        return ngram, idx_ngram
+    "We also want to know which common english ngram gave at leat T commonenglish ngrams as output"
 
-        
