19ba788e3e6c213b35e02b05a6f04e13a541f75d
HanabiOno <hanabi.ob@gmail.com>
2020-03-23 23:19:30 +0100

    improved the code

--- otp/finalfunc.py
+++ otp/finalfunc.py
@@ -15,22 +15,34 @@ from crack import(
     str_returner,
     couldbeenglish
 )
-c0 = open("../cyphertext0.txt", "r")
-c1 = open("../cyphertext1.txt", "r")
 
-difference0 = subtract_modulo_alphabet(c0.read(), c1.read())
-difference1 = subtract_modulo_alphabet(c1.read(), c0.read())
+with open("../cyphertext0.txt", "r") as cypher0:
+    c0 = cypher0.read()
+with open("../cyphertext1.txt", "r") as cypher1:
+    c1 = cypher1.read()
 
-print("difference1 is:", c1.read(), c0.read(), subtract_modulo_alphabet(c1.read(), c0.read()))
+difference0 = subtract_modulo_alphabet(c0, c1)
+difference1 = subtract_modulo_alphabet(c1, c0)
 
 plaintext1 = '-'*len(difference0)
 plaintext2 = '-'*len(difference0)
 
 finalstring1 = str_indexer('-'*len(difference0))
-cont = 'yes'
-while cont == 'yes':
+
+# After fifty iterations/trigrams you get the following words
+# keys are wordstartindex and values are the word
+fitytrigrams = {94: ['UNSAFE'], 633: ['RATTY'], 658: ['VAINLY'], 943: ['RADIATE'], 949: ['DACTYL'], 260: ['YAK'], 374: ['BIZ'], 390: ['IDEA'], 407: ['STARTS'], 708: ['RE'], 790: ['YOGI'], 443: ['SHAFT'], 550: ['ILIAD'], 549: ['WILIER'], 100: ['AT'], 649: ['PA'], 106: ['WORDY'], 480: ['TORI'], 886: ['MANHUNTS'], 680: ['IT'], 141: ['IT'], 846: ['LYRA'], 53: ['SOS'], 125: ['EM'], 340: ['POACH'], 7: ['RHEA'], 758: ['IS'], 932: ['STUB'], 700: ['PA'], 488: ['BRITTLE'], 224: ['DO'], 299: ['BORAX']}
+for index in fiftytrigrams:
+    finalstring1 = replacer(fiftytrigrams[index], index, finalstring1)
+print(str_returner(finalstring1))
+
+finalstring1 = str_indexer('-'*len(difference0))
+
+j = 0
+while j < 50:
     with open('../english_trigrams.txt', 'r') as trigrams:
         trigrams = trigrams.readlines()
+        couldbeword = {} #safe all words that have english outcome and only use those with value 1, to be sure
         for trigram in trigrams:
             "current trigram to add to difference0"
             cur_trigram = trigram[:3]
@@ -51,9 +63,19 @@ while cont == 'yes':
                     wherengraminword = word.find(upper) #where in the word is ngram
                     wordstartindex = key - wherengraminword #at what index should word start
                     d = addwordtodatindex(difference1, word, wordstartindex)
-                    print(word, wordstartindex)
-                    if couldbeenglish(d) == True: #if the whole word makes sense we move on
-                        replacer(word, wordstartindex, finalstring1)
-                        break
-            print(str_returner(finalstring1))
-            cont = input("Type yes if you want to continue to next trigram")
+                    if couldbeenglish(d) == True: #if the word gives a english outcome it is good
+                    # There are occurences where there are more then 1 couldbeenglish possibilities at the same wordstartindex
+                    # To make sure we have the right one, we only safe it when there is only 1 option
+                        if wordstartindex in couldbeword:
+                            del couldbeword[wordstartindex]
+                        else:
+                            couldbeword[wordstartindex] = [word]
+            if j%10 == 0:
+                print(j)
+                print(couldbeword)
+            j += 1
+
+
+for index in couldbeword:
+    finalstring1 = replacer(couldbeword[index], index, finalstring1)
+print(str_returner(finalstring1))
