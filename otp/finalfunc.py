db25356b63bfaba2c611c17b0c1f935f00917ae7
HanabiOno <hanabi.ob@gmail.com>
2020-03-25 12:14:16 +0100

    added print statement to see what is going on

--- otp/finalfunc.py
+++ otp/finalfunc.py
@@ -29,6 +29,7 @@ plaintext2 = '-'*len(difference0)
 
 finalstring1 = str_indexer('-'*len(difference0))
 
+'''
 # After fifty iterations/trigrams you get the following words
 # keys are wordstartindex and values are the word
 fiftytrigrams = {94: ['UNSAFE'], 633: ['RATTY'], 658: ['VAINLY'], 943: ['RADIATE'], 949: ['DACTYL'], 260: ['YAK'], 374: ['BIZ'], 390: ['IDEA'], 407: ['STARTS'], 708: ['RE'], 790: ['YOGI'], 443: ['SHAFT'], 550: ['ILIAD'], 549: ['WILIER'], 100: ['AT'], 649: ['PA'], 106: ['WORDY'], 480: ['TORI'], 886: ['MANHUNTS'], 680: ['IT'], 141: ['IT'], 846: ['LYRA'], 53: ['SOS'], 125: ['EM'], 340: ['POACH'], 7: ['RHEA'], 758: ['IS'], 932: ['STUB'], 700: ['PA'], 488: ['BRITTLE'], 224: ['DO'], 299: ['BORAX']}
@@ -37,9 +38,10 @@ for index in fiftytrigrams:
 print(str_returner(finalstring1))
 
 finalstring1 = str_indexer('-'*len(difference0))
+'''
 
 j = 0
-while j < 50:
+while j < 5:
     with open('../english_trigrams.txt', 'r') as trigrams:
         trigrams = trigrams.readlines()
         couldbeword = {} #safe all words that have english outcome and only use those with value 1, to be sure
@@ -50,12 +52,14 @@ while j < 50:
             a = addwordtod(difference0, cur_trigram)
             b = {} #dic we will use for english ngrams
             i = 0
-            print("trigram is", cur_trigram)
             while i < len(a): #i is the current ngram we are checking
                 if isngram(a[i]): #Check if ngram is an english ngram
                     b[i] =  a[i] #filling the dic with the ngrams with their index as key
                 i += 1
-            print('the english ngrams from a', b)
+            print('------------------------------------------------------------------------------------')
+            print('Commong english ngrams after addition',cur_trigram,'to difference0 (keys are index):')
+            print(b)
+            print("Following passes couldbeenglish after addition findwordswithsequence(commong eng ngram above) to difference1:")
             for key in b: #We'll now go through the english ngrams we found
                 c = findwordswithsequence(b[key]) #would return english words with that ngram
                 for word in c: #lets go through the words
@@ -66,16 +70,16 @@ while j < 50:
                     if couldbeenglish(d) == True: #if the word gives a english outcome it is good
                     # There are occurences where there are more then 1 couldbeenglish possibilities at the same wordstartindex
                     # To make sure we have the right one, we only safe it when there is only 1 option
+                        print(d, 'wordofsequence is:', word)
                         if wordstartindex in couldbeword:
                             del couldbeword[wordstartindex]
                         else:
                             couldbeword[wordstartindex] = [word]
-            if j%10 == 0:
-                print(j)
-                print(couldbeword)
+            if j%1 == 0:
+                print("These wordstartindices only have one option:", couldbeword)
+                finalstring1 = str_indexer('-'*len(difference0))
+                for index in couldbeword:
+                    finalstring1 = replacer(couldbeword[index], index, finalstring1)
+                print("How the finalstring looks now:", str_returner(finalstring1))
+                print('end of loop nr:', j+1)
             j += 1
-
-
-for index in couldbeword:
-    finalstring1 = replacer(couldbeword[index], index, finalstring1)
-print(str_returner(finalstring1))
